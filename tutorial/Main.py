e8b38f710eff42282e9e87d0fd49dd8f677ae6f8
itezcan <itezcan@uos.de>
2023-02-19 19:43:42 +0100

    use pretrained AlexNet
    torch hub loader doesn't load the pretrained AlexNet unless it's used
    with 'pretrained=True'. add that, so that we use AlexNet pretrained on
    ImageNet data instead of untrained model.
    
    increases our score from 14 to 40 on the test dataset.

--- tutorial/Main.py
+++ tutorial/Main.py
@@ -14,10 +14,11 @@ device = 'mps'  # @param ['cpu', 'cuda'] {allow-input: true}
 device = torch.device(device)
 
 # Load pretrained AlexNet
-model = torch.hub.load('pytorch/vision:v0.10.0', 'alexnet')
+# model = torch.hub.load('pytorch/vision:v0.10.0', 'alexnet')
+model = torch.hub.load('pytorch/vision:v0.10.0', 'alexnet', pretrained=True)
 model.to(device)  # send the model to the chosen device ('cpu' or 'cuda')
 model.eval()  # set the model to evaluation mode, since you are not training it
-model_name = 'alexnet_pt'
+model_name = 'alexnet_pt_true_all_layers'
 
 # See what layers AlexNet has
 train_nodes, _ = get_graph_node_names(model)
@@ -25,34 +26,36 @@ print(train_nodes)
 
 layers = ["features.2", "features.5", "features.7", "features.9", "features.12", "classifier.2", "classifier.5",
           "classifier.6"]
+# layers = ["features.9", "features.2", "classifier.2", "classifier.5", "classifier.6"]
 # layers = train_nodes
 # layers = ['features.12']
 batch_size = 300
 # subjects = [1]
 subjects = [1, 2, 3, 4, 5, 6, 7, 8]
-with open(f'{parent_output_dir}/{model_name}/results.txt', 'a') as results_file:
-    for layer_name in layers:
-        print(f'Running for layer {layer_name}')
-        for subj in subjects:
-            print(f'Running for subject {subj}')
+for layer_name in layers:
+    print(f'Running for layer {layer_name}')
+    for subj in subjects:
+        print(f'Running for subject {subj}')
 
-            # Set data directories based on parameters
-            output_dir = f'{parent_output_dir}/{model_name}/{layer_name}'
-            dataset = NSDDataset(data_dir, output_dir, subj, batch_size)
+        # Set data directories based on parameters
+        output_dir = f'{parent_output_dir}/{model_name}/{layer_name}'
+        dataset = NSDDataset(data_dir, output_dir, subj, batch_size)
 
-            # Get dataloaders
-            random_seed = 5
-            dataloaders = dataset.get_data_loaders(random_seed, device)
+        # Get dataloaders
+        random_seed = 5
+        dataloaders = dataset.get_data_loaders(random_seed, device)
 
-            # Extract features from layer, apply PCA and fit linear encoder to get fMRI predictions
-            lh_fmri_val_pred, rh_fmri_val_pred = LinearEncoder.predict(model, layer_name, dataset, dataloaders)
-            # Calculate correlations for each hemisphere
-            lh_correlation = Correlations.calculate_correlation(lh_fmri_val_pred, dataset.lh_fmri_val)
-            rh_correlation = Correlations.calculate_correlation(rh_fmri_val_pred, dataset.rh_fmri_val)
-            print(f'Left hemisphere median correlation: {np.median(lh_correlation)}')
-            print(f'Right hemisphere median correlation: {np.median(rh_correlation)}')
-            # Plot and save correlation graph
-            plot_file = f'correlations.png'
-            Correlations.plot_correlations(dataset.data_dir, lh_correlation, rh_correlation, plot_file)
-            print(f'Layer: {layer_name} Subject: {subj}', file=results_file)
-            print(f'LH Correlation: {lh_correlation} RH Correlation: {rh_correlation}', file=results_file)
+        # Extract features from layer, apply PCA and fit linear encoder to get fMRI predictions
+        lh_fmri_val_pred, rh_fmri_val_pred = LinearEncoder.predict(model, layer_name, dataset, dataloaders)
+        # Calculate correlations for each hemisphere
+        lh_correlation = Correlations.calculate_correlation(lh_fmri_val_pred, dataset.lh_fmri_val)
+        rh_correlation = Correlations.calculate_correlation(rh_fmri_val_pred, dataset.rh_fmri_val)
+        print(f'Left hemisphere median correlation: {np.median(lh_correlation)}')
+        print(f'Right hemisphere median correlation: {np.median(rh_correlation)}')
+        # Plot and save correlation graph
+        plot_file = f'{dataset.var_plot_dir}/correlations.png'
+        Correlations.plot_correlations(dataset.data_dir, lh_correlation, rh_correlation, plot_file)
+        result_file = open(f'{parent_output_dir}/{model_name}/results.txt', 'a')
+        result_file.write(f'Layer: {layer_name} Subject: {subj}\n')
+        result_file.write(f'LH Correlation: {np.median(lh_correlation)} RH Correlation: {np.median(rh_correlation)}\n')
+        result_file.close()
