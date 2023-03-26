f1ad07bfcc89e971a8b0cb00aa4f67bd4898c2b8
itezcan <itezcan@uos.de>
2023-03-26 13:52:14 +0200

    feat: merge stream predictions before individual rois
    streams like 'early' or 'ventral' are bigger ROIs that contain individual
    ones like 'V1v', 'V1d' etc.
    they were excluded when merging, since for example doing 'early' after
    'V1v' would overwrite 'V1v'.
    but streams may have indices that don't correspond to individual ROIs, so
    using them may improve final predictions.
    
    - instead of manually giving a list of ROI names, fetch ROI names from ROI
    mappings
    - load stream files first, so that streams will be processed first, then
    individual ROIs can be written over them
    - improve variable names and docstring for readability

--- src/algonauts/evaluators/predictions.py
+++ src/algonauts/evaluators/predictions.py
@@ -7,7 +7,7 @@ from src.algonauts.utils.file import save_predictions
 
 def merge_predictions_for_all_subjects(subjects, data_dir, exp_output_dir):
     """
-    Merge the best predictions for each ROI, to create a merged prediction file
+    Merge predictions from the best layers for each ROI, to create merged prediction files for given subjects
     :param subjects: list of subjects (eg. [1, 2])
     :param data_dir: folder containing challenge data
     :param exp_output_dir: folder containing output submission data
@@ -22,27 +22,27 @@ def merge_predictions_for_all_subjects(subjects, data_dir, exp_output_dir):
 
 def merge_predictions_per_subject(subj, roi_indices, exp_output_dir):
     """
-    Merge the best predictions for each ROI for the given subject, to create a merged prediction file
+    Merge predictions from the best layers for each ROI for the given subject, to create a merged prediction file
     Prediction data for each ROI will be taken from the best layer
-    Will first get predictions from the best overall layer, then write over the ROIs
+    Will first get predictions from the best layer for all vertices, then write over the ROIs
     :param subj: subject (eg. 1)
     :param roi_indices: dictionary of format {'LH': {roi_name: roi_idxs}, 'RH': {roi_name: roi_idx}}
     :param exp_output_dir: folder to fetch prediction files for the layers
-    :return: final version of predictions
+    :return:
     """
-    # Define which ROIs we are interested in
-    rois_to_use = ['V1v', 'V1d', 'V2v', 'V2d', 'V3v', 'V3d', 'hV4', 'EBA', 'FBA-1', 'FBA-2', 'mTL-bodies', 'OFA',
-                   'FFA-1', 'FFA-2', 'mTL-faces', 'aTL-faces', 'OPA', 'PPA', 'RSC', 'OWFA', 'VWFA-1', 'VWFA-2',
-                   'mfs-words', 'mTL-words']
+    # Get list of ROI names
+    roi_names = [roi_name for roi_name in roi_indices['LH'].keys()]
 
     # Get dictionary of layers with the best correlations per ROI
     best_correlations = find_best_correlations(f'{exp_output_dir}/results.json', subj)
 
-    # Get names of all layers that we will use
+    # Get names of all layers inside the best correlations map
     layers = set([hemi_val['layer'] for hemi_info in best_correlations.values() for hemi_val in hemi_info.values()])
 
     # Load predictions for the layers
     layer_predictions = get_layer_predictions(exp_output_dir, layers, subj)
+
+    # Get predictions of the best layer for all vertices and set it as default predictions
     lh_best_overall_layer = best_correlations['LH']['All vertices']['layer']
     rh_best_overall_layer = best_correlations['RH']['All vertices']['layer']
 
@@ -50,7 +50,7 @@ def merge_predictions_per_subject(subj, roi_indices, exp_output_dir):
     rh_default_predictions = layer_predictions[rh_best_overall_layer]['RH']
 
     # Merge predictions
-    for roi in rois_to_use:
+    for roi in roi_names:
         lh_idx = roi_indices['LH'][roi]
         rh_idx = roi_indices['RH'][roi]
 
@@ -66,7 +66,7 @@ def merge_predictions_per_subject(subj, roi_indices, exp_output_dir):
             rh_default_predictions[:, rh_idx] = rh_roi_best_predictions
 
     subj = f'subj{format(subj, "02")}'
-    subject_submission_dir = f'{exp_output_dir}/best/{subj}'
+    subject_submission_dir = f'{exp_output_dir}/best_layers_merged/{subj}'
     print(f'Saving merged predictions to {subject_submission_dir}')
     save_predictions(lh_default_predictions, rh_default_predictions, subject_submission_dir)
 
@@ -94,7 +94,7 @@ def get_layer_predictions(exp_output_dir, layers, subj):
 
 def load_roi_indices(data_dir, subj):
     """
-    Get the ROI indices per each hemisphere for the given subject
+    Get a map of "roi_name: roi_indices" for each ROI in each hemisphere for the given subject
     :param data_dir: folder containing challenge data, used to fetch ROI mappings
     :param subj: subject (eg. 1)
     :return: dictionary with format {'LH': {'roi_name': roi_idx}, 'RH': {'roi_name': roi_idx}}
@@ -102,23 +102,23 @@ def load_roi_indices(data_dir, subj):
     subj = f'subj{format(subj, "02")}'
     roi_data_dir = f'{data_dir}/{subj}'
     # Load the ROI classes mapping dictionaries
-    roi_mapping_files = ['mapping_prf-visualrois.npy', 'mapping_floc-bodies.npy',
+    roi_mapping_files = ['mapping_streams.npy', 'mapping_prf-visualrois.npy', 'mapping_floc-bodies.npy',
                          'mapping_floc-faces.npy', 'mapping_floc-places.npy',
-                         'mapping_floc-words.npy', 'mapping_streams.npy']
+                         'mapping_floc-words.npy']
     roi_name_maps = []
     for r in roi_mapping_files:
         roi_name_maps.append(np.load(os.path.join(roi_data_dir, 'roi_masks', r),
                                      allow_pickle=True).item())
 
     # Load the ROI brain surface maps
-    lh_challenge_roi_files = ['lh.prf-visualrois_challenge_space.npy',
+    lh_challenge_roi_files = ['lh.streams_challenge_space.npy', 'lh.prf-visualrois_challenge_space.npy',
                               'lh.floc-bodies_challenge_space.npy', 'lh.floc-faces_challenge_space.npy',
                               'lh.floc-places_challenge_space.npy', 'lh.floc-words_challenge_space.npy',
-                              'lh.streams_challenge_space.npy']
-    rh_challenge_roi_files = ['rh.prf-visualrois_challenge_space.npy',
+                              ]
+    rh_challenge_roi_files = ['rh.streams_challenge_space.npy', 'rh.prf-visualrois_challenge_space.npy',
                               'rh.floc-bodies_challenge_space.npy', 'rh.floc-faces_challenge_space.npy',
                               'rh.floc-places_challenge_space.npy', 'rh.floc-words_challenge_space.npy',
-                              'rh.streams_challenge_space.npy']
+                              ]
     lh_challenge_rois = []
     rh_challenge_rois = []
     for r in range(len(lh_challenge_roi_files)):
@@ -127,18 +127,14 @@ def load_roi_indices(data_dir, subj):
         rh_challenge_rois.append(np.load(os.path.join(roi_data_dir, 'roi_masks',
                                                       rh_challenge_roi_files[r])))
 
-    # Select the correlation results vertices of each ROI
-    roi_names = []
+    # Create map of roi_name: roi_indices for each ROI in each hemisphere
     roi_indices = {'LH': {}, 'RH': {}}
-    for r1 in range(len(lh_challenge_rois)):
-        for r2 in roi_name_maps[r1].items():
-            if r2[0] != 0:  # zeros indicate to vertices falling outside the ROI of interest
-                roi_names.append(r2[1])
-                lh_roi_idx = np.where(lh_challenge_rois[r1] == r2[0])[0]
-                rh_roi_idx = np.where(rh_challenge_rois[r1] == r2[0])[0]
-                roi_indices['LH'][r2[1]] = lh_roi_idx
-                roi_indices['RH'][r2[1]] = rh_roi_idx
-                print(f'{r2[1]}')
-    roi_names.append('All vertices')
-    print(f'roi names: {roi_names}')
+    for roi_type_id in range(len(lh_challenge_rois)):
+        for roi_id, roi_name in roi_name_maps[roi_type_id].items():
+            if roi_id != 0:  # zeros indicate to vertices falling outside the ROI of interest
+                lh_roi_idx = np.where(lh_challenge_rois[roi_type_id] == roi_id)[0]
+                rh_roi_idx = np.where(rh_challenge_rois[roi_type_id] == roi_id)[0]
+                roi_indices['LH'][roi_name] = lh_roi_idx
+                roi_indices['RH'][roi_name] = rh_roi_idx
+                print(f'{roi_name}')
     return roi_indices
